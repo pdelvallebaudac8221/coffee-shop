c533ecc87f2640dbf43c7e4b9136d7fdfa97ea58
pdelvallebaudac8221 <pdelvallebaudac8221@users.noreply.github.com>
2025-04-05 01:23:08 +0000

    deploy: ff0bb1762570bb7376871a097a48748adac43dcf

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,13 @@
-console.log('main.js ready!');
+$(document).ready(function(){
+  $('.slick-carousel').slick({
+    autoplay: true,
+    arrows: false,
+    easing: 'ease-out',
+    autoplaySpeed: 2500,
+    pauseOnHover: false,
+  });
+
+  const cart = localStorage.getItem('cart');
+  console.log(window.location.href.includes('/menu'));
+
+});
